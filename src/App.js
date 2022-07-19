8236391b4c701919ab382e61392a7ada7e14b9da
raoufzouaoui <raoufzouaouo06@gmail.com>
2022-07-19 23:34:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const sounds=[
+  {key:'Q',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'},
+  {key:'W',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'},
+  {key:'E',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'},
+  {key:'A',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'},
+  {key:'S',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},
+  {key:'D',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'},
+  {key:'Z', mp3:'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'},
+  {key:'X', mp3:'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
+  {key:'C',
+  mp3:'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'},
+]
+
+const keys= ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+const sound=[ 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' , 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'  
+]
+
+ const App = (props) => (
+        <div id="display" className="display">
+           {sounds.map((sound,idx)=> (
+           <Box text={sound.key} key={idx} audio={sound.mp3}/>
+        ))}
+        </div>
+    )
+
+class Box extends React.Component{
+     constructor(props) {
+    super(props);
+    this.audio = React.createRef();
+  }
+  playSound = () => { 
+    this.audio.current.play();
+  }
+  render(){
+    const { text , audio } = this.props;
+    return(
+      <div className="drum-pad box " onClick={this.playSound}>{text}
+        <audio ref={this.audio} src={audio} id={text} className="clip"/>
     </div>
-  );
+    )
+  }
 }
+  
+
+
 
 export default App;
